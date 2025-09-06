33a898a7e7f3f75f71726fa4026a458f799129b4
TheGeniusOfEternity <max06safin@yandex.ru>
2025-09-07 00:08:25 +0300

    feat(auth resolver): register method

--- src/api/resolvers/auth/auth.resolver.ts
+++ src/api/resolvers/auth/auth.resolver.ts
@@ -3,11 +3,24 @@ import {useRouter} from "vue-router";
 import {PreRegisterInputDto} from "./dto/input/pre-register-input.dto";
 import {CommonOutputDto} from "../../dto/common-output.dto";
 import {LoginInputDto} from "./dto/input/login-input.dto";
-import {LoginMessageOutputDto} from "./dto/output/login-message-output.dto";
+import {JWTMessageOutputDto} from "./dto/output/jwt-message-output.dto";
+import {UserRegistrationDto, UserWithChildRegistrationDto} from "@/api/resolvers/auth/dto/input/register-input.dto";
 
 export class AuthResolver {
     private apiResolver = new ApiResolver()
     private router = useRouter()
+
+    public async register(data: UserRegistrationDto | UserWithChildRegistrationDto){
+        return await this
+            .apiResolver
+            .request<UserRegistrationDto | UserWithChildRegistrationDto,
+                CommonOutputDto<JWTMessageOutputDto | string>>(
+                    "users-service/v1/auth/register",
+                "POST",
+                data,
+                null
+            )
+    }
 
     public async preRegister(data: PreRegisterInputDto) {
         return await this
@@ -23,7 +36,7 @@ export class AuthResolver {
     public async login(data: LoginInputDto) {
         return await this
             .apiResolver
-            .request<LoginInputDto, CommonOutputDto<LoginMessageOutputDto | string>>(
+            .request<LoginInputDto, CommonOutputDto<JWTMessageOutputDto | string>>(
                 "users-service/v1/auth/login",
                 "POST",
                 data,
--- src/api/resolvers/auth/dto/output/jwt-message-output.dto.ts
+++ src/api/resolvers/auth/dto/output/jwt-message-output.dto.ts
@@ -1,4 +1,4 @@
-export interface LoginMessageOutputDto {
+export interface JWTMessageOutputDto {
     accessToken: string;
     refreshToken: string;
 }
