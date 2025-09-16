3d7794a324fc860738d0931954a6ab62fd36bfe1
TheGeniusOfEternity <max06safin@yandex.ru>
2025-09-16 23:38:01 +0300

    fix(docs templates): download links

--- src/views/tutor/TutorDocuments.vue
+++ src/views/tutor/TutorDocuments.vue
@@ -255,8 +255,8 @@ export default {
       docTemplates: [
         { label: "Конкурсное задание ОЧНОГО отборочного этапа", link: "task_offline_template.docx" },
         { label: "Конкурсное задание ОНЛАЙН отборочного этапа", link: "task_online_template.docx" },
-        { label: "Лист регистрации для очных мероприятий", link: "criteria_template.xlsx" },
-        { label: "Критерии оценки", link: "registration_list_offline_events_template.docx" },
+        { label: "Лист регистрации для очных мероприятий", link: "registration_list_offline_events_template.docx" },
+        { label: "Критерии оценки", link: "criteria_template.xlsx" },
       ],
       errors: {
         toastPopup: {
