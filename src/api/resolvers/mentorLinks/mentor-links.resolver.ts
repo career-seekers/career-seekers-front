c08a90cfcd2fd56622a2ac3cbad31a7e2dda03c5
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-10-01 03:45:31 +0300

    fix(api): fixed mentor links api resolver

--- src/api/resolvers/mentorLinks/mentor-links.resolver.ts
+++ src/api/resolvers/mentorLinks/mentor-links.resolver.ts
@@ -1,51 +1,93 @@
 import ApiResolver from '@/utils/ApiResolver';
-import type { CommonOutputDto } from '@/api/dto/common-output.dto';
-import type { CreateMentorLinkInputDto } from './dto/input/create-mentor-link-input.dto';
-import type { MentorLinkOutputDto } from './dto/output/mentor-link-output.dto';
+import type {CommonOutputDto} from '@/api/dto/common-output.dto';
+import type {MentorLinkOutputDto} from '@/api/resolvers/mentorLinks/dto/output/mentor-link-output.dto.ts';
+import type {CreateMentorLinkInputDto} from "@/api/resolvers/mentorLinks/dto/input/create-mentor-link-input.dto.ts";
 
 export class MentorLinksResolver {
   private apiResolver = new ApiResolver("users-service/v1/mentor-links");
   private token = localStorage.getItem("access_token");
 
-  public async create(data: CreateMentorLinkInputDto) {
-    return await this.apiResolver.request<
-      CreateMentorLinkInputDto,
-      CommonOutputDto<MentorLinkOutputDto | string>
-    >("", "POST", data, this.token ? this.token : undefined);
+  public async getAll() {
+    return this.apiResolver.request<
+      null,
+      CommonOutputDto<MentorLinkOutputDto[]>
+    >(
+      "/",
+      "GET",
+      null,
+      this.token ? this.token : undefined
+    );
+  }
+
+  public async getById(id: number) {
+    return this.apiResolver.request<
+      null,
+      CommonOutputDto<MentorLinkOutputDto>
+    >(
+      `/${id}`,
+      "GET",
+      null,
+      this.token ? this.token : undefined
+    );
   }
 
   public async getByUserId(userId: number) {
-    return await this.apiResolver.request<
+    return this.apiResolver.request<
       null,
-      CommonOutputDto<MentorLinkOutputDto | string>
-    >(`getByUserId/${userId.toString()}`, "GET", null, this.token ? this.token : undefined);
+      CommonOutputDto<MentorLinkOutputDto>
+    >(
+      `/getByUserId/${userId}`,
+      "GET",
+      null,
+      this.token ? this.token : undefined
+    );
   }
 
   public async getByBiscuit(biscuit: string) {
-    return await this.apiResolver.request<
+    return this.apiResolver.request<
       null,
-      CommonOutputDto<MentorLinkOutputDto | string>
-    >(`getByBiscuit/${biscuit}`, "GET", null, this.token ? this.token : undefined);
-  }
-
-  public async assignMentorToParent(data: { parentId: number; mentorId: number }) {
-    return await this.apiResolver.request<
-      { parentId: number; mentorId: number },
-      CommonOutputDto<string>
-    >("assign", "POST", data, this.token ? this.token : undefined);
-  }
-
-  public async getParentMentors(parentId: number) {
-    return await this.apiResolver.request<
+      CommonOutputDto<MentorLinkOutputDto>
+    >(
+      `/getByBiscuit/${biscuit}`,
+      "GET",
       null,
-      CommonOutputDto<MentorLinkOutputDto[] | string>
-    >(`parent/${parentId.toString()}/mentors`, "GET", null, this.token ? this.token : undefined);
+      this.token ? this.token : undefined
+    );
   }
 
-  public async removeMentorFromParent(data: { parentId: number; mentorId: number }) {
-    return await this.apiResolver.request<
-      { parentId: number; mentorId: number },
+  public async create(item: CreateMentorLinkInputDto) {
+    return this.apiResolver.request<
+      CreateMentorLinkInputDto,
+      CommonOutputDto<MentorLinkOutputDto>
+    >(
+      `/`,
+      "POST",
+      item,
+      this.token ? this.token : undefined
+    );
+  }
+
+  public async deleteById(id: number) {
+    return this.apiResolver.request<
+      null,
       CommonOutputDto<string>
-    >("remove", "DELETE", data, this.token ? this.token : undefined);
+    >(
+      `/${id}`,
+      "DELETE",
+      null,
+      this.token ? this.token : undefined
+    )
+  }
+
+  public async deleteAll() {
+    return this.apiResolver.request<
+      null,
+      CommonOutputDto<string>
+    >(
+      `/`,
+      "DELETE",
+      null,
+      this.token ? this.token : undefined
+    )
   }
 }
