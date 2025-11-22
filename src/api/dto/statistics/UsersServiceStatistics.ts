f62299c6375f16053f188c159ca5de6d261bb75f
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-11-23 00:03:22 +0300

    feat(store): updated statistics store

--- src/api/dto/statistics/UsersServiceStatistics.ts
+++ src/api/dto/statistics/UsersServiceStatistics.ts
@@ -1,1 +1,12 @@
-export interface UsersServiceStatistics {}
+export interface UsersServiceStatistics {
+  tutorsInfo: UsersStatisticPairDto | null,
+  expertsInfo: UsersStatisticPairDto | null,
+  mentorsInfo: UsersStatisticPairDto | null,
+  usersInfo: UsersStatisticPairDto | null,
+  childrenCount: number | null,
+}
+
+export interface UsersStatisticPairDto {
+  count: number,
+  verified: number,
+}
--- src/stores/statisticsStore.ts
+++ src/stores/statisticsStore.ts
@@ -1,6 +1,7 @@
 import {defineStore} from "pinia";
 import type {CommonStatisticsInterface} from "@/api/dto/statistics/CommonStatisticsInterface.ts";
 import type {EventServiceStatistics} from "@/api/dto/statistics/EventServiceStatistics.ts";
+import type {UsersServiceStatistics} from "@/api/dto/statistics/UsersServiceStatistics.ts";
 
 export const statisticsStore = defineStore("statistics", {
   state: (): CommonStatisticsInterface => ({
@@ -10,7 +11,14 @@ export const statisticsStore = defineStore("statistics", {
     directionsWithoutDocs: null,
     directionDocsCount: null,
     lastDocumentUpload: null,
+
+    tutorsInfo: null,
+    expertsInfo: null,
+    mentorsInfo: null,
+    usersInfo: null,
+    childrenCount: null,
   }),
+
   getters: {
     getPlatformsCount: (state: CommonStatisticsInterface) => state.platformsCount,
     getVerifiedPlatformsCount: (state: CommonStatisticsInterface) => state.platformsCount,
@@ -18,7 +26,14 @@ export const statisticsStore = defineStore("statistics", {
     getDirectionsWithoutDocs: (state: CommonStatisticsInterface) => state.directionsWithoutDocs,
     getDirectionDocsCount: (state: CommonStatisticsInterface) => state.directionDocsCount,
     getLastDocumentUpload: (state: CommonStatisticsInterface) => state.lastDocumentUpload,
+
+    getTutorsInfo: (state: CommonStatisticsInterface) => state.tutorsInfo,
+    getExpertsInfo: (state: CommonStatisticsInterface) => state.expertsInfo,
+    getMentorsInfo: (state: CommonStatisticsInterface) => state.mentorsInfo,
+    getUsersInfo: (state: CommonStatisticsInterface) => state.usersInfo,
+    getChildrenCount: (state: CommonStatisticsInterface) => state.childrenCount,
   },
+
   actions: {
     updateEventsServiceStatistics(value: EventServiceStatistics) {
       this.platformsCount = value.platformsCount;
@@ -27,6 +42,14 @@ export const statisticsStore = defineStore("statistics", {
       this.directionsWithoutDocs = value.directionsWithoutDocs
       this.directionDocsCount = value.directionDocsCount;
       this.lastDocumentUpload = value.lastDocumentUpload;
+    },
+
+    updateUsersServiceStatistics(value: UsersServiceStatistics) {
+      this.tutorsInfo = value.tutorsInfo;
+      this.expertsInfo = value.expertsInfo;
+      this.mentorsInfo = value.mentorsInfo;
+      this.usersInfo = value.usersInfo;
+      this.childrenCount = value.childrenCount;
     }
   }
 })
