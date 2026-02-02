8543ff852fdf9f48149e0fa0815d9fa786933701
TheGeniusOfEternity <max06safin@yandex.ru>
2026-02-02 16:50:56 +0300

    feat(docs|templates): add new files

--- src/shared/UseDocumentTemplates.ts
+++ src/shared/UseDocumentTemplates.ts
@@ -1,8 +1,10 @@
 export const useDocumentTemplates = [
   { label: "Конкурсное задание ОЧНОГО отборочного этапа", link: "task_offline_template.docx" },
   { label: "Конкурсное задание ОНЛАЙН отборочного этапа", link: "task_online_template.docx" },
+  { label: "Конкурсное задание финального этапа", link: "task_final_template.docx" },
   { label: "Лист регистрации для очных мероприятий", link: "registration_list_offline_events_template.docx" },
-  { label: "Критерии оценки", link: "criteria_template.xlsx" },
+  { label: "Критерии оценки отборочного этапа", link: "criteria_qualifying_template.xlsx" },
+  { label: "Критерии оценки финального этапа", link: "criteria_final_template.xlsx" },
   { label: "Описание компетенции", link: "competence_description_template.docx" },
   { label: "Конкурсное задание ФИНАЛА дошкольных компетенций", link: "task_final_preschool_competence_template.docx" },
   { label: "Итоговая ведомость отборочного этапа", link: "total_qualifying_statement.xlsx" },
