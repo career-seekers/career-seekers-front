3fef34083e7bb47b81ea06e20c427c9d4cb5be63
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-04-05 02:59:15 +0300

    refactor(types): extract error types to external class

---
+++ src/shared/errors/ParentFormErrorsTypes.ts
@@ -0,0 +1,16 @@
+export type ParentFormErrorsTypes = {
+  lastName: string;
+  firstName: string;
+  patronymic: string;
+  birthDate: string;
+  relationship: string;
+  phone: string;
+  telegramLink: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  childConsentFile: string;
+  consent: string;
+  agreement: string;
+  rules: string;
+}
